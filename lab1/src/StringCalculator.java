15635ee697e66f986945a8352a109d4480b6c31d
Damir Zaichenko <zaichenkodamir@gmail.com>
2022-09-27 09:27:19 +0300

    bugfix

--- lab1/src/StringCalculator.java
+++ lab1/src/StringCalculator.java
@@ -1,66 +1,84 @@
 import java.util.*;
 
 class StringCalculator {
-
     public static boolean isNumeric(String str) {
         try {
-            Integer.parseInt(str);
+            Double.parseDouble(str);
             return true;
-        } catch (NumberFormatException e) {
+        } catch(NumberFormatException e){
             return false;
         }
     }
-
-    static int add(String numbers) {
+    int add(String input) {
         List<String> delimiters = new ArrayList<>();
+        String[] del_temp = {",", "\n"};
 
-        delimiters.add(",");
-        delimiters.add("\\n");
+        delimiters.add(del_temp[0]);
+        delimiters.add(del_temp[1]);
 
-        if (numbers.isEmpty()) return 0;
-        else if (numbers.startsWith("//")) {
-            if (Objects.equals('[', numbers.charAt(2))) {
-                Collections.addAll(delimiters, numbers.substring(3, numbers.indexOf("]\\n")).split("]\\Q["));
-                numbers = numbers.substring(numbers.indexOf("]\\n") + 3);
-            } else {
-                delimiters.add((numbers.substring(2, numbers.indexOf("\\n"))));
-                numbers = numbers.substring(numbers.indexOf("\\n") + 2);
+        if (Objects.equals(input, "")) return 0;
+
+        else if (input.startsWith("//")) {
+            if (Objects.equals("[", input.substring(2,3))) {
+                for (int i = 0; i < input.length()-2; i++) {
+                    if (Objects.equals(input.substring(i, i+3), "]\\n")) {
+                        del_temp = input.substring(3, i).split("]\\Q[");
+                        for (int j = 0; j < del_temp.length; j++) {
+                            delimiters.add(del_temp[j]);
+                        }
+                        input = input.substring(i+3);
+                    }
+                }
+            }
+            else {
+                for (int i = 0; i < input.length()-1; i++) {
+                    if (Objects.equals(input.substring(i, i+2), "\\n")) {
+                        delimiters.add(input.substring(2, i));
+                        input = input.substring(i+2);
+                    }
+                }
             }
             Collections.sort(delimiters);
             Collections.reverse(delimiters);
         }
 
-        for (String delimiter : delimiters) {
-            numbers = numbers.replaceAll("\\Q" + delimiter + "\\E", ",");
+        System.out.println(input); //выпилить
+
+
+        for (int i = 0; i < delimiters.size(); i++) {
+            input = input.replaceAll("\\Q" + delimiters.get(i) + "\\E", "*");
         }
 
-        if (numbers.contains(",,")) throw new IllegalArgumentException("Two or more delimiters together");
+        if (input.contains("**")) throw new IllegalArgumentException("two delimiters in a row");
 
         int result = 0;
-        String[] numbersList = numbers.split(",");
-        List<String> negativeNums = new ArrayList<>();
+        String[] nums = input.split("\\Q*");
+        List<String> negative_nums = new ArrayList<>();
 
-        for (String num : numbersList) {
-            if (isNumeric(num)) {
-                if (Integer.parseInt(num) < 0) negativeNums.add(num);
-                else if (Integer.parseInt(num) <= 1000) result += Integer.parseInt(num);
-            } else throw new IllegalArgumentException("Not allowed delimiter");
+        for (int i = 0; i < nums.length; i++) {
+            if (isNumeric(nums[i])) {
+                if (Integer.parseInt(nums[i]) > 1000) continue;
+                else if (Integer.parseInt(nums[i]) < 0) negative_nums.add(nums[i]);
+                else result += Integer.parseInt(nums[i]);
+            }
+            else throw new IllegalArgumentException("wrong delimiter");
         }
 
-        if (negativeNums.size() != 0) {
-            throw new IllegalArgumentException("Negative numbers: " + negativeNums);
-        }
+        if (negative_nums.size()!=0) throw new IllegalArgumentException("negative nums: " + negative_nums);
 
         return result;
+
     }
 }
+
 
 
 class Main {
     public static void main(String[] args) {
         System.out.print("Enter string: ");
         Scanner read = new Scanner(System.in);
-        String inputNumbers = read.nextLine();
-        System.out.println(StringCalculator.add(inputNumbers));
+        String str = read.nextLine();
+        StringCalculator calc = new StringCalculator();
+        System.out.println(calc.add(str));
     }
 }
